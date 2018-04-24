14a6c42f2c557cd4eb76d6c887c0a88c3ed0668d
jmellado <je_in_er@hotmail.com>
2018-04-24 12:14:12 -0500

    Version 14.4
    - Se agregaron las variables de session(nombres, apellido1, apellido2),
    para el rol administrador.

--- application/views/roles/rol_administrador_vista.php
+++ application/views/roles/rol_administrador_vista.php
@@ -173,7 +173,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?php echo base_url(); ?>assets/plantillas/AdminLTE/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs"><?php echo $this->session->userdata('nombres')?> <?php echo $this->session->userdata('apellido1')?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -181,7 +181,7 @@
                 <img src="<?php echo base_url(); ?>assets/plantillas/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
+                  <?php echo $this->session->userdata('nombres')?> <?php echo $this->session->userdata('apellido1')?> <?php echo $this->session->userdata('apellido2')?> - Administrador
                   <small>Member since Nov. 2012</small>
                 </p>
               </li>
@@ -232,7 +232,7 @@
           <img src="<?php echo base_url(); ?>assets/plantillas/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p><?php echo $this->session->userdata('nombres')?> <?php echo $this->session->userdata('apellido1')?> <?php echo $this->session->userdata('apellido2')?></p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
